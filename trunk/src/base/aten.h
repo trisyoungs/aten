7d721f19d105339df055326952ca9aaf975bd920
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2008-08-20 09:06:55 +0000

    Small fixes to code. Bug in CommandList::topmostBranch() fixed which caused crashes with strict compilers.

--- trunk/src/base/aten.h
+++ trunk/src/base/aten.h
@@ -30,8 +30,8 @@
 #include "templates/namemap.h"
 
 #define ATENVERSION "0.99"
-#define ATENREVISION "566"
-#define ATENDATE "Tue 19 Aug - 16:19"
+#define ATENREVISION "567"
+#define ATENDATE "Wed 20 Aug - 10:05"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 // Forward Declarations
