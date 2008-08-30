c7236b93d0ff07a915f7643f435d9d68c6274c8c
t.youngs <t.youngs@qub.ac.uk>
2008-08-30 11:33:35 +0000

    Moved src/parse/expression* to src/command, and src/parse/forcefield.cpp to src/ff/loadforcefield.cpp. Updated Makefile.ams and CMakeLists.txt in various places.

--- src/aten/aten.h
+++ src/aten/aten.h
@@ -30,7 +30,7 @@
 
 #define ATENVERSION "0.99"
 #define ATENREVISION "583"
-#define ATENDATE "Sat 30 Aug - 12:50"
+#define ATENDATE "Sat 30 Aug - 12:56"
 #define ATENURL "http://aten.googlecode.com/svn/branches/newvar"
 
 // Forward Declarations
