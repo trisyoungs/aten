be14367cc28d6a2f78c821237e36845eb4df03b6
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2008-10-25 22:23:15 +0000

    Version correcting commit.

--- trunk/src/main/aten.h
+++ trunk/src/main/aten.h
@@ -28,8 +28,8 @@
 #include "templates/namemap.h"
 
 #define ATENVERSION "0.99"
-#define ATENREVISION "695"
-#define ATENDATE "Sat 25 Oct - 23:16"
+#define ATENREVISION "697"
+#define ATENDATE "Sat 25 Oct - 23:23"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 // Forward Declarations
