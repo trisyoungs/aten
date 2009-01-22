3dbcfdeb2f9863937bac6b1c317aca50c4cdd9e6
t.youngs <t.youngs@qub.ac.uk>
2009-01-22 13:51:39 +0000

    Some test code in surface rendering routines had been inadvertently committed - repaired in this revision.

--- src/main/aten.h
+++ src/main/aten.h
@@ -28,8 +28,8 @@
 #include "templates/namemap.h"
 
 #define ATENVERSION "1.1"
-#define ATENREVISION "770"
-#define ATENDATE "Thu 22 Jan - 12:32"
+#define ATENREVISION "771"
+#define ATENDATE "Thu 22 Jan - 13:51"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 // Forward Declarations
