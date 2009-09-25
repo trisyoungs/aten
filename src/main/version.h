3e3687a1d8eaf3fc8cb5809f5d9bc4f5d3549b26
t.youngs <t.youngs@qub.ac.uk>
2009-09-25 16:15:29 +0000

    Added 'selectradius' command.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.4"
-#define ATENREVISION "1008"
-#define ATENDATE "Mon 21 Sep - 16:37"
+#define ATENREVISION "1009"
+#define ATENDATE "Fri 25 Sep - 17:15"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
