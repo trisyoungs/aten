2475a24e39f699ab17bf9f9ab83b8c8f79d39aef
t.youngs <t.youngs@qub.ac.uk>
2009-11-15 17:15:57 +0000

    Previous commit also added missing access to 'typenames' in PatternBound variable. Fixed - Decrement operators (--) for pointer types. Adding missing MeasurementVariable code to StepNode. Added/Fixed - Measurements in AKF filter.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.4"
-#define ATENREVISION "1031"
-#define ATENDATE "Sun 15 Nov - 17:03"
+#define ATENREVISION "1032"
+#define ATENDATE "Sun 15 Nov - 17:16"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
