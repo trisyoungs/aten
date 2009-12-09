e0113bb9d3dc0a032d9e1987a95ed3a90527c232
t.youngs <t.youngs@qub.ac.uk>
2009-12-09 17:38:58 +0000

    Fixed detection of planar atoms. Fixed - If impropers were detected in a pattern other than the very first, atom IDs were calculated incorrectly for the expression and a segfault would occur.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1060"
-#define ATENDATE "Wed 09 Dec - 16:49"
+#define ATENREVISION "1061"
+#define ATENDATE "Wed 09 Dec - 17:39"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
