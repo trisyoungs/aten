c0e156bf722b8208a8d821f7add7d89eebcab873
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2009-12-24 15:29:43 +0000

    Docs again.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1075"
-#define ATENDATE "Thu 24 Dec - 13:37"
+#define ATENREVISION "1076"
+#define ATENDATE "Thu 24 Dec - 15:29"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
