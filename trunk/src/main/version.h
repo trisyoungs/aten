13ef9a653ef1f40d60d38736d0e7949d9f90cb14
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-01-13 19:01:38 +0000

    Adjusted old terms in oplsaa.ff (UA methylenechloride, AA alkyl chlorides) and added more new ones (sulfoxide, some sulfonamides).

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1101"
-#define ATENDATE "Wed 13 Jan - 17:40"
+#define ATENREVISION "1102"
+#define ATENDATE "Wed 13 Jan - 19:02"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
