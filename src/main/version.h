9f8ce9fe5118f14775c34cb4fd1f2b137c576b7e
t.youngs <t.youngs@qub.ac.uk>
2010-01-21 16:44:23 +0000

    Fixed lww-il.ff symmetric ring-carbon cation types for bmim and emim.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1105"
-#define ATENDATE "Thu 21 Jan - 12:31"
+#define ATENREVISION "1106"
+#define ATENDATE "Thu 21 Jan - 16:45"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
