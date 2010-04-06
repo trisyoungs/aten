827928eda32afe1d8a705fa99b24805bc6063f53
t.youngs <t.youngs@qub.ac.uk>
2010-04-06 11:23:08 +0000

    Tweaks to oplsaa.ff. Moved 'scripts' directory to installed data dir. Doc update.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1143"
-#define ATENDATE "Sun 04 Apr - 16:54"
+#define ATENREVISION "1144"
+#define ATENDATE "Tue 06 Apr - 12:23"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
