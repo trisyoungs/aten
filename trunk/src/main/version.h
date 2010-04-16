2fd19b82124c33a40ab7b9ee87d34172035f3947
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-04-16 18:34:24 +0000

    Added src/classes/neta_grammar.cc and src/classes/neta_grammar.h to repo, to make Win/Mac builds easier.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1162"
-#define ATENDATE "Fri 16 Apr - 19:19"
+#define ATENREVISION "1163"
+#define ATENDATE "Fri 16 Apr - 19:34"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
