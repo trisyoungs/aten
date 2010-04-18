29198a1e4b65f23f67008dd87b569ba8196353c7
t.youngs <t.youngs@qub.ac.uk>
2010-04-18 11:41:27 +0000

    Small fixes for build service (aten.dsc, configure.ac).

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1175"
-#define ATENDATE "Sun 18 Apr - 12:30"
+#define ATENREVISION "1176"
+#define ATENDATE "Sun 18 Apr - 12:41"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
