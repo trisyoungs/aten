ca6529e7e0ad2c17f7a5751ba7c72572fb0e1b30
t.youngs <t.youngs@qub.ac.uk>
2010-04-18 10:11:23 +0000

    Fixed version in specfile.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.5"
-#define ATENREVISION "1169"
-#define ATENDATE "Sun 18 Apr - 10:33"
+#define ATENREVISION "1170"
+#define ATENDATE "Sun 18 Apr - 11:11"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
