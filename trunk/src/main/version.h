d54b28ab5eac51f2e8f6df3eb4c45d287f1342cf
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-05-18 14:01:52 +0000

    Fixed - Crash when element conversion gave a number that was out range. Forcefield load window now begins in Aten's data directory. Fixed - dlpoly FIELD filter was referencing 'ljopls' instead of 'ljgeom'.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1201"
-#define ATENDATE "Thu 13 May - 14:09"
+#define ATENREVISION "1203"
+#define ATENDATE "Tue 18 May - 15:01"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
