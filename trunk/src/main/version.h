f3bbcaca26751894fc75a0978153e9ffa9864791
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-07-19 12:41:08 +0000

    Removed 'leftMargin', 'rightMargin', 'topMargin', and 'bottomMargin' propert definitions from QBoxLayouts in glyphs.ui, grids.ui, and position.ui since they do not get moc'd properly with the Qt version supplied in RHEL (no other ui files contained the definitions).

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1220"
-#define ATENDATE "Mon 19 Jul - 11:25"
+#define ATENREVISION "1221"
+#define ATENDATE "Mon 19 Jul - 13:41"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
