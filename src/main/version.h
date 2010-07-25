8219e63f6e87878e63782d343be7eca27b5b5ca5
t.youngs <t.youngs@qub.ac.uk>
2010-07-25 17:11:23 +0000

    Fixed - Pattern detection was broken in a previous commit. Doc update - old tutorials now included in examples manual.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1225"
-#define ATENDATE "Thu 22 Jul - 17:05"
+#define ATENREVISION "1226"
+#define ATENDATE "Sun 25 Jul - 18:11"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
