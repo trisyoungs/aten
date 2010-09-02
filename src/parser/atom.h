22f6b99292dc1b477ef941d272ff5020b83a0b13
t.youngs <t.youngs@qub.ac.uk>
2010-09-02 13:13:23 +0000

    Added 'style' accessor to Atom variable. Added support for reading/writing atom style and custom colour information to akf format.

--- src/parser/atom.h
+++ src/parser/atom.h
@@ -41,7 +41,7 @@ class AtomVariable : public PointerVariable
 	*/
 	public:
 	// Accessor list
-        enum Accessors { Bonds, Colour, F, Fixed, FracX, FracY, FracZ, FX, FY, FZ, Hidden, Id, Mass, Name, NBonds, Q, R, RX, RY, RZ, Selected, Symbol, Type, V, VX, VY, VZ, Z, nAccessors };
+        enum Accessors { Bonds, Colour, F, Fixed, FracX, FracY, FracZ, FX, FY, FZ, Hidden, Id, Mass, Name, NBonds, Q, R, RX, RY, RZ, Selected, Style, Symbol, Type, V, VX, VY, VZ, Z, nAccessors };
 	// Function list
 	enum Functions { DummyFunction, nFunctions };
 	// Search variable access list for provided accessor
