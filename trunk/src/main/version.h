f3d307e1e8a811a3d9d43a7de32c9b85d0266125
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-09-07 20:22:39 +0000

    Removed annoying spurious xml headers from seversl ui files.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1258"
-#define ATENDATE "Tue 07 Sep - 21:17"
+#define ATENREVISION "1259"
+#define ATENDATE "Tue 07 Sep - 21:23"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
