b77311aaa79b0344b5674591aeb97f971bb762bb
t.youngs <t.youngs@qub.ac.uk>
2010-09-29 12:36:53 +0000

    Fixed - Aten's 'frame' accessor variable was wrongly using Model::renderSource() instead of Model::renderSourceModel().

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1283"
-#define ATENDATE "Wed 29 Sep - 09:39"
+#define ATENREVISION "1284"
+#define ATENDATE "Wed 29 Sep - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
