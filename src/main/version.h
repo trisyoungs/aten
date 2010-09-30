3056bdf09678d00aad365492af1abe5112b275ec
t.youngs <t.youngs@qub.ac.uk>
2010-09-30 11:34:20 +0000

    Fixed - Playing a trajectory to the end would lock Canvas in read-only mode.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1289"
-#define ATENDATE "Thu 30 Sep - 12:26"
+#define ATENREVISION "1290"
+#define ATENDATE "Thu 30 Sep - 12:34"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
