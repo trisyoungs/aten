67ccccf046caab3da48b12386173fd85dfbbd29d
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-10-12 09:43:22 +0000

    Fixed - Bug in torsion angle generation for rule forcefields introduced when implementing dummy term generation. Added 'expand', 'selectall', and 'selectnone' functions to ModelVariable. Made edit operations a little more verbose.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1307"
-#define ATENDATE "Mon 11 Oct - 17:51"
+#define ATENREVISION "1308"
+#define ATENDATE "Tue 12 Oct - 10:43"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
--- trunk/src/parser/model.h
+++ trunk/src/parser/model.h
@@ -43,7 +43,7 @@ class ModelVariable : public PointerVariable
 	// Accessor list
 	enum Accessors { Angles, Atoms, Bonds, Celldata, Distances, Eigenvectors, Energy, FFAngles, FFBonds, FFMass, FFTorsions, FFTypes, FField, Filename, Frame, Frames, Glyphs, Id, Mass, Name, NAngles, NAtoms, NBasisCartesians, NBasisShells, NBonds, NDistances, NEigenvectors, NFFAngles, NFFBonds, NFFTorsions, NFFTypes, NFrames, NGlyphs, NPatterns, NRequested, NSelected, NTorsions, NUnknown, NVibrations, Patterns, Region, Torsions, Vibrations, ZMatrix, nAccessors };
 	// Function list
-	enum Functions { AddHydrogen, Augment, Charge, ClearBonds, ClearCharges, ClearSelectedBonds, Copy, Cut, Delete, Finalise, MoveToEnd, MoveToStart, NewAtom, NewAtomFrac, NewBasisShell, NewBond, NewBondId, NewEigenvector, NewGlyph, NewGrid, NewVibration, Paste, ReBond, ReBondPatterns, ReBondSelection, Redo, ReOrder, SaveBitmap, SelectionAddHydrogen, ShiftDown, ShiftUp, ShowAll, ToAngstroms, Transmute, Undo, nFunctions };
+	enum Functions { AddHydrogen, Augment, Charge, ClearBonds, ClearCharges, ClearSelectedBonds, Copy, Cut, Delete, Expand, Finalise, MoveToEnd, MoveToStart, NewAtom, NewAtomFrac, NewBasisShell, NewBond, NewBondId, NewEigenvector, NewGlyph, NewGrid, NewVibration, Paste, ReBond, ReBondPatterns, ReBondSelection, Redo, ReOrder, SaveBitmap, SelectAll, SelectionAddHydrogen, SelectNone, ShiftDown, ShiftUp, ShowAll, ToAngstroms, Transmute, Undo, nFunctions };
 
 	// Search variable access list for provided accessor
 	StepNode *findAccessor(const char *s, TreeNode *arrayindex, TreeNode *arglist = NULL);
