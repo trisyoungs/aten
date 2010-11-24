713ed1cd3034e9962f54ef49d5098b2808c2797c
t.youngs <t.youngs@qub.ac.uk>
2010-11-24 15:52:07 +0000

    Fixed - Some discrepancies between context menu atom IDs and those sent to commands. Minor tweak to grammar.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1365"
-#define ATENDATE "Fri 19 Nov - 14:13"
+#define ATENREVISION "1373"
+#define ATENDATE "Wed 24 Nov - 15:52"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
