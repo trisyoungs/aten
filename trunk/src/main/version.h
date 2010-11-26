83806cf66f2540bd90a1be1982d504d17232440a
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-11-26 22:44:25 +0000

    Fixed - Erroneous output from 'measureselected' command.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1373"
-#define ATENDATE "Wed 24 Nov - 15:52"
+#define ATENREVISION "1375"
+#define ATENDATE "Fri 26 Nov - 22:44"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
