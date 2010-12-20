041a938193b4c1a8dcfb057b59d6de6e6f25dcd9
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2010-12-20 11:16:03 +0000

    Fixed - Windows build was not generating zip package.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1402"
-#define ATENDATE "Thu 09 Dec - 17:22"
+#define ATENREVISION "1420"
+#define ATENDATE "Mon 20 Dec - 11:16"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
