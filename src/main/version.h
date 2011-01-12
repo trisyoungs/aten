c1b3291681fb404d31fed2b04248d7046952b9ac
t.youngs <t.youngs@qub.ac.uk>
2011-01-12 15:02:18 +0000

    Reverted to old path structure on website, changing doc build script to match.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1439"
-#define ATENDATE "Tue 11 Jan - 21:40"
+#define ATENREVISION "1440"
+#define ATENDATE "Wed 12 Jan - 15:03"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
