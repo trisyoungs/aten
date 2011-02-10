4b4021803acbd2795ba3032d6c6bceefe719a7e7
t.youngs <t.youngs@qub.ac.uk>
2011-02-10 13:22:36 +0000

    Update cif filter to handle basics of mmCIF.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1467"
-#define ATENDATE "Thu 10 Feb - 12:15"
+#define ATENREVISION "1468"
+#define ATENDATE "Thu 10 Feb - 13:22"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
