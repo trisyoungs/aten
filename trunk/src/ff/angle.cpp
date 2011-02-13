c294949956c9e63dfb2a2239dd84fb4962da7608
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-02-13 23:06:55 +0000

    Partial update of years in copyright notice in source files (ff, main).

--- trunk/src/ff/angle.cpp
+++ trunk/src/ff/angle.cpp
@@ -1,7 +1,7 @@
 /*
 	*** Angle energy / force calculation
 	*** src/ff/angle.cpp
-	Copyright T. Youngs 2007-2010
+	Copyright T. Youngs 2007-2011
 
 	This file is part of Aten.
 
--- trunk/src/main/aten_filters.cpp
+++ trunk/src/main/aten_filters.cpp
@@ -1,7 +1,7 @@
 /*
 	*** Aten Filter-Specific Routines
 	*** src/main/aten_filters.cpp
-	Copyright T. Youngs 2007-2010
+	Copyright T. Youngs 2007-2011
 
 	This file is part of Aten.
 
