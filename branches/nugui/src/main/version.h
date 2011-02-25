49321f9a3834c427490a108527b69370351a3be0
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-02-25 20:28:47 +0000

    Renamed fragment* to fragments* in src/gui. Converted some more tool windows to dock widgets.

--- branches/nugui/src/main/version.h
+++ branches/nugui/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.7"
-#define ATENREVISION "1499"
-#define ATENDATE "Fri 25 Feb - 18:53"
+#define ATENREVISION "1500"
+#define ATENDATE "Fri 25 Feb - 20:29"
 #define ATENURL "http://aten.googlecode.com/svn/branches/nugui"
 
 #endif
