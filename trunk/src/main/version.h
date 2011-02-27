265ef3f8a5452ebed5f77aadbd971c3fd7fc3ce1
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-02-27 22:07:29 +0000

    Tweaks to changelog1.6 and doc build script (to remove aggravating xml header in single-page manual).

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.7"
-#define ATENREVISION "1510"
-#define ATENDATE "Sun 27 Feb - 20:22"
+#define ATENREVISION "1512"
+#define ATENDATE "Sun 27 Feb - 22:07"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
