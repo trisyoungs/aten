518576fc29f3cd32bb4c92683e8e312ecc93d252
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-02-28 12:38:12 +0000

    Minor change to Ring class to prevent misuse of potentially undefined value.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.7"
-#define ATENREVISION "1512"
-#define ATENDATE "Sun 27 Feb - 22:07"
+#define ATENREVISION "1518"
+#define ATENDATE "Mon 28 Feb - 12:38"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
