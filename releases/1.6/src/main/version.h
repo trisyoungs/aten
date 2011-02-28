b3177b6420c3e23e9ffa45438843cc81f44f6ed0
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-02-28 14:09:20 +0000

    Fixed another silly error with QTVERSION variable in configure.ac, arising from previous error fix.

--- releases/1.6/src/main/version.h
+++ releases/1.6/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1520"
-#define ATENDATE "Mon 28 Feb - 13:49"
+#define ATENREVISION "1521"
+#define ATENDATE "Mon 28 Feb - 14:09"
 #define ATENURL "http://aten.googlecode.com/svn/releases/1.6"
 
 #endif
