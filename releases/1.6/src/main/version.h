ad2ef3a0beb8c5f10d996c3b0ac54d165b967ce1
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-02-28 10:01:39 +0000

    Modified debian.changelog to attempt to fix wrong naming of generated packages. Removed random 'leftMargin' property from src/gui/prefs.ui to attempt to fix builds on distros with slightly older Qt (e.g. RHEL_5). Tweaked aten.dsc in the hope of fixing some builds (but which will probably break others).

--- releases/1.6/src/main/version.h
+++ releases/1.6/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.6"
-#define ATENREVISION "1511"
-#define ATENDATE "Sun 27 Feb - 21:25"
+#define ATENREVISION "1514"
+#define ATENDATE "Mon 28 Feb - 10:02"
 #define ATENURL "http://aten.googlecode.com/svn/releases/1.6"
 
 #endif
