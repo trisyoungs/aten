435377b7489dbff2621b8800d61178845ece5382
t.youngs <t.youngs@qub.ac.uk>
2011-04-03 09:41:47 +0000

    Fixed - --noqtsettings switch was broken. Fixed - Grid origin group had default widget title.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.7"
-#define ATENREVISION "1603"
-#define ATENDATE "Fri 01 Apr - 17:13"
+#define ATENREVISION "1604"
+#define ATENDATE "Sun 03 Apr - 10:41"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
