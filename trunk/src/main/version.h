2d2c0667b9b37346cb099340d42795dee33dddfc
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-05-17 08:05:54 +0000

    Updated todo. Minor fix in mainwindow_funcs.cpp (printf format).

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.7"
-#define ATENREVISION "1644"
-#define ATENDATE "Mon 16 May - 09:15"
+#define ATENREVISION "1645"
+#define ATENDATE "Tue 17 May - 09:07"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
