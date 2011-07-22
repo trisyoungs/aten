7c92ef866c5358d0a7526f829e8f8506b48dd59e
t.youngs <t.youngs@qub.ac.uk>
2011-07-22 13:43:07 +0000

    Minor doc update. Updated commit script.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.8"
-#define ATENREVISION "1705"
-#define ATENDATE "Fri 22 Jul - 11:56"
+#define ATENREVISION "1706"
+#define ATENDATE "Fri 22 Jul - 14:45"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
