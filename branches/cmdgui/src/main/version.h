af112d934b0fbc9f71d2a24e42e8769561fda4df
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-10-12 15:02:57 +0000

    Tweaked gamess filter.

--- branches/cmdgui/src/main/version.h
+++ branches/cmdgui/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.8"
-#define ATENREVISION "1736"
-#define ATENDATE "Wed 12 Oct - 16:02"
+#define ATENREVISION "1737"
+#define ATENDATE "Wed 12 Oct - 16:06"
 #define ATENURL "http://aten.googlecode.com/svn/branches/cmdgui"
 
 #endif
