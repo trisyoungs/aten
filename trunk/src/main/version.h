7dccc7338a113ae625d0459ffd2f29031ffa1d1c
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2011-10-25 10:18:10 +0000

    More tweaks to gamess output filter.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.8"
-#define ATENREVISION "1744"
-#define ATENDATE "Tue 25 Oct - 10:42"
+#define ATENREVISION "1745"
+#define ATENDATE "Tue 25 Oct - 11:21"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
