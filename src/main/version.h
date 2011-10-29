5392e93db33f7718e25a55f57dab1c38d9a41d06
t.youngs <t.youngs@qub.ac.uk>
2011-10-29 15:56:38 +0000

    Updated TODO.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.8"
-#define ATENREVISION "1751"
-#define ATENDATE "Fri 28 Oct - 13:28"
+#define ATENREVISION "1752"
+#define ATENDATE "Sat 29 Oct - 16:56"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
