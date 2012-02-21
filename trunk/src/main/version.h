22d1b52d16bd2234a93dffe506b96f31b1edf38b
t.youngs@qub.ac.uk <t.youngs@qub.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2012-02-21 19:04:28 +0000

    Fixed - Minor issue which meant that default locations were searched even if ATENDATA was set.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.8"
-#define ATENREVISION "1777"
-#define ATENDATE "Thu 26 Jan - 11:00"
+#define ATENREVISION "1778"
+#define ATENDATE "Tue 21 Feb - 19:04"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
