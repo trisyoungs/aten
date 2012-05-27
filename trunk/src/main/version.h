acdc964530224bfb22aeb9c60a110c00dc2fad2f
trisyoungs@googlemail.com <trisyoungs@googlemail.com@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2012-05-27 20:06:42 +0000

    Tweaks to aten.dsc.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.8"
-#define ATENREVISION "1814"
-#define ATENDATE "Sun 27 May - 20:56"
+#define ATENREVISION "1815"
+#define ATENDATE "Sun 27 May - 21:07"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
