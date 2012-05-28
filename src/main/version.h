07a3addc09b7a8ea603465a9e7e931eaa3166dc1
trisyoungs <trisyoungs@googlemail.com>
2012-05-28 14:20:34 +0000

    Tweaked aten.dsc, fixing all current Debian-based builds. Tweaked build.ui to fix RH5-based builds.

--- src/main/version.h
+++ src/main/version.h
@@ -23,8 +23,8 @@
 #define ATEN_VERSION_H
 
 #define ATENVERSION "1.8"
-#define ATENREVISION "1815"
-#define ATENDATE "Sun 27 May - 21:07"
+#define ATENREVISION "1816"
+#define ATENDATE "Mon 28 May - 15:21"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
 #endif
