9b65cb1795f014c964baf2effe31b9a4af2d7ff4
trisyoungs <trisyoungs@googlemail.com>
2012-07-14 14:43:21 +0000

    Updated TODO.

--- aten.spec
+++ aten.spec
@@ -4,7 +4,7 @@
 # Name, brief description, and version 
 Summary: Aten - Atomic configuration builder and editor
 Name: %{shortname}
-Version: 1.827
+Version: 1.828
 Release: 1
 License: GPL
 %define fullname %{name}-%{version}
--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.827"
-#define ATENREVISION "1827"
+#define ATENVERSION "1.828"
+#define ATENREVISION "1828"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
