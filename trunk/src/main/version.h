5551c497832d831e99a5d4c51143995cbbf5b083
trisyoungs@googlemail.com <trisyoungs@googlemail.com@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2012-07-26 19:51:48 +0000

    Fixed - Extra debug output removed from RenderEngine::renderUserActions().

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.830"
-#define ATENREVISION "1830"
+#define ATENVERSION "1.832"
+#define ATENREVISION "1832"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
