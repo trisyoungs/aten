36dc2468ce88d44de3388dcaed612f0fdc2c21ae
tristan.youngs@stfc.ac.uk <tristan.youngs@stfc.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2012-07-31 18:21:59 +0000

    Fixed - Manipulation / editing of trajectory frames was largely broken due to commands being wrongly sent to parent Model instead.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.832"
-#define ATENREVISION "1832"
+#define ATENVERSION "1.833"
+#define ATENREVISION "1833"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
