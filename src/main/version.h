6d32b6988edb8e96862a2b23468d28e08942ad4f
trisyoungs <trisyoungs@googlemail.com>
2012-11-23 13:06:46 +0000

    Fixed similar bison make error in src/parser.

--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.840"
-#define ATENREVISION "1840"
+#define ATENVERSION "1.841"
+#define ATENREVISION "1841"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
