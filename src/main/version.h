a8073adba50b7d59d05a59877a00a0dcaff3096c
trisyoungs <trisyoungs@googlemail.com>
2012-11-23 14:05:00 +0000

    Added missing src/classes/neta_grammar.hh to repo, to fix Windows build.

--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.841"
-#define ATENREVISION "1841"
+#define ATENVERSION "1.842"
+#define ATENREVISION "1842"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
