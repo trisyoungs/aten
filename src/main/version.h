8fe58a7344b58b266f9d4bf0a8cb40019c48bd22
tristan.youngs <tristan.youngs@stfc.ac.uk>
2013-07-23 19:41:09 +0000

    PDB export filter ATOM format updated for better conformity..

--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.851"
-#define ATENREVISION "1851"
+#define ATENVERSION "1.852"
+#define ATENREVISION "1852"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
