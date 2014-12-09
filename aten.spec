fcbbd509a742c066491196a87bcd87d26491d222
tristan.youngs <tristan.youngs@stfc.ac.uk>
2014-12-09 10:02:46 +0000

    Updated QuantumEspresso filter to read MD trajectory from output.

--- aten.spec
+++ aten.spec
@@ -4,7 +4,7 @@
 # Name, brief description, and version 
 Summary: Aten - Atomic configuration builder and editor
 Name: %{shortname}
-Version: 1.880
+Version: 1.881
 Release: 1
 License: GPL
 %define fullname %{name}-%{version}
--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.880"
-#define ATENREVISION "1880"
+#define ATENVERSION "1.881"
+#define ATENREVISION "1881"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
