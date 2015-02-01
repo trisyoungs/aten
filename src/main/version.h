f4030a76256a0523c2ed2868ff3407b656fb033b
tristan.youngs <tristan.youngs@stfc.ac.uk>
2015-02-01 09:33:03 +0000

    Bump to r1884.

--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.883"
-#define ATENREVISION "1883"
+#define ATENVERSION "1.884"
+#define ATENREVISION "1884"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
