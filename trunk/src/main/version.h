517f363e408cc9cd4b96f8224908855ee0f5f90e
tristan.youngs@stfc.ac.uk <tristan.youngs@stfc.ac.uk@9e9e25b3-ed3b-0410-ab63-e9a3d03df1e2>
2015-02-01 09:34:33 +0000

    Bump to r1890.

--- trunk/src/main/version.h
+++ trunk/src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.889"
-#define ATENREVISION "1889"
+#define ATENVERSION "1.890"
+#define ATENREVISION "1890"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
