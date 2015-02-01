543c0a9ce9aa9a055dfd0eb9a1228dd5849bf2ea
tristan.youngs <tristan.youngs@stfc.ac.uk>
2015-02-01 09:34:16 +0000

    Bump to r1888.

--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.887"
-#define ATENREVISION "1887"
+#define ATENVERSION "1.888"
+#define ATENREVISION "1888"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
