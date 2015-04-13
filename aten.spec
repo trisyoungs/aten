75ef97f67935335dbb40f9c72d1929fcc936d9ac
tristan.youngs <tristan.youngs@stfc.ac.uk>
2015-04-13 21:37:56 +0000

    Fixed - Dialog filter list strings were not updated on filter load, meaning menu items were not enabled. Fixed - Unit cell was drawn in wrong position.

--- aten.spec
+++ aten.spec
@@ -4,7 +4,7 @@
 # Name, brief description, and version 
 Summary: Aten - Atomic configuration builder and editor
 Name: %{shortname}
-Version: 1.947
+Version: 1.948
 Release: 1
 License: GPL
 %define fullname %{name}-%{version}
--- src/main/version.h
+++ src/main/version.h
@@ -22,8 +22,8 @@
 #ifndef ATEN_VERSION_H
 #define ATEN_VERSION_H
 
-#define ATENVERSION "1.947"
-#define ATENREVISION "1947"
+#define ATENVERSION "1.948"
+#define ATENREVISION "1948"
 #define ATENDATE "Thu 28 Jun - 13:36"
 #define ATENURL "http://aten.googlecode.com/svn/trunk"
 
