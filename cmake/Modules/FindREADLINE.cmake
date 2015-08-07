fea3ac041190593d2cd382965b82ec21e09bbfd4
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2015-08-07 11:21:26 +0100

    Cleaned up root CMakeLists. Added CMake module to find glext.h, and updated FindREADLINE.cmake.

--- cmake/Modules/FindREADLINE.cmake
+++ cmake/Modules/FindREADLINE.cmake
@@ -1,22 +1,48 @@
-IF(APPLE)
-FIND_PATH(READLINE_INCLUDE_DIR readline/readline.h /usr/include/readline /usr/local/include/readline ENV INCLUDE)
-FIND_LIBRARY(READLINE_LIBRARY NAMES readline PATHS /usr/lib /usr/local/lib /usr/lib64 ENV DYLD_LIBRARY_PATH) 
-ELSE(APPLE)
-FIND_PATH(READLINE_INCLUDE_DIR readline/readline.h /usr/include/readline /usr/local/include/readline ENV INCLUDE)
-FIND_LIBRARY(READLINE_LIBRARY NAMES readline PATHS /usr/lib /usr/local/lib /usr/lib64 ENV LD_LIBRARY_PATH) 
-ENDIF(APPLE)
-IF(WIN32)
-FIND_PATH(READLINE_INCLUDE_DIR readline/readline.h /usr/include/readline /usr/local/include/readline ENV INCLUDE )
-FIND_LIBRARY(READLINE_LIBRARY NAMES readline PATHS /usr/lib /usr/local/lib /usr/lib64 ENV LIB) 
-ENDIF(WIN32)
+# - Locate readline library and headers
+# This module defines
+#  READLINE_LIBRARIES, the library to link against
+#  READLINE_FOUND, if false, do not try to link to readline
+#  READLINE_INCLUDE_DIRS, where to find headers.
+#
+# $READLINE_DIR is an environment variable that points to the directory in which the readline lib resides.
 
-IF (READLINE_INCLUDE_DIR AND READLINE_LIBRARY)
-   SET(READLINE_FOUND TRUE)
-ENDIF (READLINE_INCLUDE_DIR AND READLINE_LIBRARY)
+FIND_PATH(READLINE_INCLUDE_DIR readline/readline.h 
+  HINTS
+  $ENV{READLINE_DIR}
+  PATH_SUFFIXES include src
+  PATHS
+  /usr/include
+  /usr/local/include
+  /sw/include
+  /opt/local/include
+  /usr/freeware/include
+)
 
+FIND_LIBRARY(READLINE_LIBRARY
+  NAMES readline libreadline readline_static
+  HINTS
+  $ENV{READLINE_DIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS
+  /usr/lib
+  /usr/local/lib
+  /sw
+  /usr/freeware
+)
+
+# set the user variables
+IF(READLINE_INCLUDE_DIR)
+  SET(READLINE_INCLUDE_DIRS "${READLINE_INCLUDE_DIR}")
+ENDIF()
+SET(READLINE_LIBRARIES "${READLINE_LIBRARY}")
+
+# handle the QUIETLY and REQUIRED arguments and set READLINE_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE("FindPackageHandleStandardArgs")
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(READLINE  DEFAULT_MSG  READLINE_INCLUDE_DIR  READLINE_LIBRARY)
 
 IF (READLINE_FOUND)
-      MESSAGE(STATUS "Found READLINE: ${READLINE_LIBRARY}")
-ELSE (READLINE_FOUND)
-      MESSAGE(FATAL_ERROR "Could not find READLINE")
+	MESSAGE(STATUS "Found READLINE: ${READLINE_LIBRARIES}")
 ENDIF (READLINE_FOUND)
+
+MARK_AS_ADVANCED(READLINE_INCLUDE_DIR)
