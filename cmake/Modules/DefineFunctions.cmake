7a1f543ad2d4ce2cb6c6eb0bdff565a18ce351fa
Alin M Elena <alinm.elena@gmail.com>
2016-12-04 17:44:48 +0000

    [cmake] add mopac options to cmake, also deal more elegantly with the c++11 need

--- cmake/Modules/DefineFunctions.cmake
+++ cmake/Modules/DefineFunctions.cmake
@@ -1,18 +1,26 @@
 function(INSTALL_TARGET _TAR _TAR_PUBLIC_HEADERS)
-	install(TARGETS
-		${_TAR}
-		RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-		LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-		ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-	)
+  install(TARGETS
+    ${_TAR}
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    )
 
-	foreach(header ${${_TAR_PUBLIC_HEADERS}})
-		file(RELATIVE_PATH relative_path ${CMAKE_SOURCE_DIR}/src ${CMAKE_CURRENT_SOURCE_DIR}/${header})
-		get_filename_component(directories ${relative_path} DIRECTORY)
-		install(FILES
-			${header}
-			DESTINATION
-			${CMAKE_INSTALL_INCLUDEDIR}/aten/${directories}
-		)
-	endforeach()
+  foreach(header ${${_TAR_PUBLIC_HEADERS}})
+    file(RELATIVE_PATH relative_path ${CMAKE_SOURCE_DIR}/src ${CMAKE_CURRENT_SOURCE_DIR}/${header})
+    get_filename_component(directories ${relative_path} DIRECTORY)
+    install(FILES
+      ${header}
+      DESTINATION
+      ${CMAKE_INSTALL_INCLUDEDIR}/aten/${directories}
+      )
+  endforeach()
 endfunction(INSTALL_TARGET)
+
+macro(use_cxx11)
+  if (CMAKE_VERSION VERSION_LESS "3.1")
+    message(WARNING "Be sure you add c++11 support to your compiler")
+  else ()
+    set (CMAKE_CXX_STANDARD 11)
+  endif ()
+endmacro(use_cxx11)
